989d3e4cd1566a052f1482920fc65d384a7b887b
shahzod <shahzod3137@gmail.com>
2025-12-05 00:18:50 +0500

    fix: manifest and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,7 +33,10 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
+      link: [
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        { rel: "manifest", href: "/manifest.webmanifest" },
+      ],
     },
   },
 
